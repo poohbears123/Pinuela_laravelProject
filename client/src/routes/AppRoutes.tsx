2ea680ff041b66eae253377a3b9636990e9aa414
unknown <rjmeldogello6@gmail.com>
2026-03-29 19:11:32 +0800

    added sidebar and header and its function

---
+++ client/src/routes/AppRoutes.tsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom"
+import AppLayout from "../layout/AppLayout"
+
+const SampleComponent = () => {
+    return (
+        <>
+            <h1 className="text-red-500 bg-amber-300">Hello World</h1>
+        </>
+    )
+}
+
+const AppRoutes = () => {
+    return (
+        <>
+            <Routes>
+                <Route element={<AppLayout />}>
+                    <Route path="/" element={<SampleComponent />} />
+                </Route>
+            </Routes >
+
+        </>
+    )
+}
+
+export default AppRoutes
